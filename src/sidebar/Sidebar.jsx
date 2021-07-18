d32f0580f116179aaf57ddb7bab43b233a60c2ef
Liza-Cheptsova <cheptsova.liza@mail.ru>
2021-07-19 00:39:11 +0300

    fix

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -4,18 +4,20 @@ import s from "./Sidebar.module.css";
 export const Sidebar = () => {
   return (
     <div className={s.sidebar_container}>
-      <h1>Frontend dev</h1>
-      <nav className={s.nav}>
-        <NavLink to={"/home"} className={s.link} activeClassName={s.activeLink}>
-          Home
-        </NavLink>
-        <NavLink to={"/about"} className={s.link} activeClassName={s.activeLink}>
-          About
-        </NavLink>
-        <NavLink to={"/portfolio"} className={s.link} activeClassName={s.activeLink}>
-          Portfolio
-        </NavLink>
-      </nav>
+      <div className={s.menu}>
+        <h1>Frontend dev</h1>
+        <nav className={s.nav}>
+          <NavLink to={"/home"} className={s.link} activeClassName={s.activeLink}>
+            Home
+          </NavLink>
+          <NavLink to={"/about"} className={s.link} activeClassName={s.activeLink}>
+            About
+          </NavLink>
+          <NavLink to={"/portfolio"} className={s.link} activeClassName={s.activeLink}>
+            Portfolio
+          </NavLink>
+        </nav>
+      </div>
     </div>
   );
 };
